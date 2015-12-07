2a9a4c072d7767d8ebba2ef4d8d99ff220248ad2
jake-g <omonoid@uw.edu>
2015-12-06 16:29:06 -0800

    finished and comment task 1

--- task1.m
+++ task1.m
@@ -1,4 +1,31 @@
+close all
 load('SGroup5.mat')
-hist(StudentData.rcvd)
+r = StudentData.rcvd;
+[f,bin] = hist(r);    % bin and freq
 
-% gof
+% Compute statistics
+sig = std(r);
+mu = mean(r);
+pdf = normpdf(bin,mu, sig);
+
+dx = diff(bin(1:2));    % bin width
+bar(bin,f/sum(f*dx)); hold on % normalized hist
+plot(bin,pdf,'r');  % plot theoretical pdf
+
+% Preform gof test on theoretical pdf
+[h,p,st] = chi2gof(r)
+
+
+%% OUTPUT
+% h =  0
+% 
+% 
+% p = 6.9278e-01
+% 
+% 
+% st = 
+%     chi2stat: 3.8809e+00
+%           df: 6
+%        edges: [-6.1397e+00 -3.7330e+00 -2.5296e+00 -1.3263e+00 -1.2294e-01 1.0804e+00 2.2838e+00 3.4871e+00 4.6905e+00 5.8938e+00]
+%            O: [23 53 151 211 252 175 98 28 9]
+%            E: [2.1883e+01 6.0615e+01 1.4095e+02 2.2375e+02 2.4254e+02 1.7954e+02 9.0737e+01 3.1300e+01 8.6871e+00]
