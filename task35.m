b70dfcdb14ac28aee69c2b6f7ff929dd5a2b8697
jake-g <omonoid@uw.edu>
2015-12-08 14:41:41 -0800

    task35 works

--- task35.m
+++ task35.m
@@ -4,15 +4,15 @@ codes = [2 3 4 5 7 11 13];
 barker13 = barker(13);
 code_stats = zeros(length(codes), 3);
 for i = 1:length(codes)
-    combined_code = [];     % Combine two codes
-    for j = 1:length(barker(13))
-        combined_code =  [combined_code barker13(j)*barker(codes(i))'];
-    end
-    
+%     combined_code = [];     % Combine two codes
+%     for j = 1:length(barker(13))
+%         combined_code =  [combined_code barker13(j)*barker(codes(i))'];
+%     end
+    K = kron(barker13,barker(codes(i)));  % combines the codes  
     % Plot Correlation
     subplot(length(codes),1,i); 
     clear Rxx
-    [Rxx, peak, peak_side, mean_side] = autocorr(combined_code);
+    [Rxx, peak, peak_side, mean_side] = autocorr(K);
     plot(Rxx, 'color',rand(1,3)); hold on
     title(sprintf('Code: %i',i));  
     
