414ed2ec5fae3e42ab2e4d41340d57b3f706e49c
jake g <omonoid@uw.edu>
2015-12-11 18:46:07 -0800

    task4 plots

--- task4.m
+++ task4.m
@@ -1,12 +1,34 @@
 clear all; close all
 
+% Get results from task 2
+run('task2.m');
+run('group0_ans.m');
 load('SGroup0.mat')
+
+
 r = StudentData.rcvd;
 w = StudentData.refnoise;
+mu_r = mean(r)
+std_r = std(r)
 
-corr = abs(filter(flipud(barker(13)), 1, r));
-subplot(2,1,2); plot(corr)
-title('Filtered')
+
+thresh = 2*std_r^2 * v0(4) + mu_r
+
 subplot(2,1,1); plot(r)
 title('Original')
-winners = find((corr>25)) - 12
+corr = 12 + abs(filter(flipud(barker(13)), 1, r));
+subplot(2,1,2); plot(corr, 'm')
+winners = find((corr > thresh));
+length(winners)
+
+hold on;
+stem(winners, corr(winners))
+hold on;
+stem(epochs + 12 , corr(epochs + 12), '*');
+
+hold on;
+plot([0, length(corr)], [thresh, thresh])
+
+title('Filtered')
+xlim([0,length(corr)])
+ylim([mean(corr), max(corr)])
