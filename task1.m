dbbd631a0b6aa1246849fdb2e4107366fe9a55d6
jisooj <jisooj@uw.edu>
2015-12-15 20:51:31 -0800

    two distributions: raw and normalized

--- task1.m
+++ task1.m
@@ -14,23 +14,42 @@ for i=1:Nt
     V_RV(i) = V;
 end
 
-v = V_RV / std(V_RV);
+%------- Plot pdf for V_RV ---------------------
+[f,bin] = hist(V_RV, 25);  % bin and freq
+sig = std(V_RV);
+mu = mean(V_RV);
+pdf = normpdf(bin, mu, sig);
+dx = diff(bin(1:2));       % bin width
 
+% Plot
+figure;
+bar(bin, f / sum(f * dx), 'r'); % normalized hist
+hold on;
+plot(bin, pdf, 'r');  % plot theoretical pdf
+title('Histogram for V_R_V');
+xlabel('V_R_V');
+ylabel('Frequency');
+
+%------- Plot pdf for v = V_RV / std(V_RV) -----
+v = V_RV / std(V_RV);
 % Compute statistics'
 [f,bin] = hist(v, 25);    % bin and freq
 sig = std(v);
 mu = mean(v);
-pdf = normpdf(bin,mu, sig);
+pdf = normpdf(bin, mu, sig);
+dx = diff(bin(1:2));    % bin width
 
 % Plot
-dx = diff(bin(1:2));    % bin width
-bar(bin,f/sum(f*dx)); hold on % normalized hist
+figure;
+bar(bin, f / sum(f * dx)); % normalized hist
+hold on;
 plot(bin,pdf);  % plot theoretical pdf
-title('Histogram for v = V_R_V / std(V_R_V)');
-xlabel('V_R_V');
+title('Histogram for v, where v = V_R_V / std(V_R_V)');
+xlabel('v');
 ylabel('Frequency');
 
-% Preform gof test on theoretical pdf
+%------- chi-squared test to check normality -----
+% Perform gof test on theoretical pdf
 [h,p,st] = chi2gof(v)
 
 % Q value for recieved signal and noise ref
